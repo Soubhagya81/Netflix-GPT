653d8576f55ccfcc45cc8d59f080bb22f8c7e807
soubhgya <soubhgyarj@integramicro.co.in>
2024-06-14 09:49:37 +0530

    Login Page Added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,11 +10,13 @@ const Login = () => {
             alt='logo1'>
             </img>
         </div>
-        <div>
-          <form className='bg-black relative'>
-            <input type="text"  placeholder='Email Address' className="p-2 m-2"></input>
-            <input type="password"  placeholder="Password" className="p-2 m-2"></input>
-            <button type="submit" className="p-2 m-2 bg-white">Sign In</button>
+        <div className='flex flex-wrap justify-center items-center min-h-screen '>
+          <form className= 'py-20 px-12 w-1/5 h-3/4 relative   bg-black rounded bg-opacity-85'>
+          <h2 className='text-white font-semibold p-1 m-1 text-3xl'>Sign In</h2>
+            <input type="text"  placeholder='Email Address' className="p-2 my-2 bg-gray-700 text-white h-12 rounded w-full"></input>
+            <input type="password"  placeholder="Password" className="p-2 my-2 bg-gray-700 text-white h-12 rounded w-full"></input>
+            <button type="submit" className="p-2 my-6 items-center bg-red-700 text-white rounded w-full">Sign In</button>
+            <p className='py-2 my-2 text-white'>New to Netflix? Sign up now.</p>
           </form>
         </div>
     </div>
